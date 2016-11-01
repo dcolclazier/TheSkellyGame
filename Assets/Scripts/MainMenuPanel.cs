206ff84a4096b9d5285656badedea23c1c7c5fd3
David Colclazier <david@jdc.tech>
2016-11-01 15:04:19 -0600

    More multiplayer functionality added - nothing really working yet

---
+++ Assets/Scripts/MainMenuPanel.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuPanel : MonoBehaviour {
+
+    public MenuManager MenuManager;
+
+    public RectTransform MultiplayerPanel;
+    public RectTransform SettingsPanel;
+    
+	public void OnClickSinglePlayer()
+    {
+
+    }
+
+    public void OnClickMultiPlayer()
+    {
+        MenuManager.SwitchPanel(MultiplayerPanel);
+    }
+    public void OnClickSettings()
+    {
+        MenuManager.SwitchPanel(SettingsPanel);
+    }
+    public void OnClickExit()
+    {
+
+    }
+
+}
