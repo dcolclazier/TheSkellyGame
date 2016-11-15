97296d50cedf71f56f51d865751ecf1a6675ec66
David Colclazier <david@jdc.tech>
2016-11-15 14:02:57 -0700

    Players can now join the game (many movement bugs...) Cameras also follow local players.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,13 +1,22 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Networking;
 
 public class PlayerMovement : NetworkBehaviour {
     public Rigidbody2D Rigidbody;
     public float Speed = 5.0f;
+    public float Jump = 1500f;
+    public LayerMask GroundLayer;
+
+    private Animator _anim;
+    private float isGroundedRayLength = 0.1f;
+
     public float MovementInput { get; private set; }
 
     public void Awake() {
         Rigidbody = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
+
     }
 
     public void SetDefaults() {
@@ -17,9 +26,39 @@ public class PlayerMovement : NetworkBehaviour {
 
     private void Update() {
         if (!isLocalPlayer) return;
-        MovementInput = Input.GetAxis("Horizontal");
-    }
 
+        if (!_anim.GetBool("Death")) {
+            _anim.SetFloat("Speed", Input.GetAxisRaw("Horizontal"));
+            MovementInput = Input.GetAxisRaw("Horizontal");
+        }
+        if (Input.GetKeyDown(KeyCode.E)) {
+            _anim.SetBool("Death", true);
+            StartCoroutine(revive(5));
+        }
+        if (Input.GetKeyDown(KeyCode.Space)/* && isGrounded*/) {
+            //Rigidbody.AddForce(Vector2.up * Jump);
+            //Rigidbody.velocity += new Vector2(0, Jump);
+            Rigidbody.AddForce(Vector2.up * Jump);
+        }
+        //MovementInput = Input.GetAxis("Horizontal");
+    }
+    public bool isGrounded
+    {
+        get
+        {
+            Vector3 position = transform.position;
+            position.y = GetComponent<Collider2D>().bounds.min.y + 0.1f;
+            float length = isGroundedRayLength + 0.1f;
+            Debug.DrawRay(position, Vector3.down * length);
+            bool grounded = Physics2D.Raycast(position, Vector3.down, length, GroundLayer.value);
+            return grounded;
+        }
+    }
+    IEnumerator revive(int time)
+    {
+        yield return new WaitForSeconds(time);
+        _anim.SetBool("Death", false);
+    }
     private void FixedUpdate() {
         if(!isLocalPlayer) return;
         Move();
--- Assets/PlayerSetup.cs
+++ Assets/PlayerSetup.cs
@@ -2,6 +2,7 @@
 using UnityEngine.UI;
 using System.Collections;
 using UnityEngine.Networking;
+using UnityStandardAssets._2D;
 
 public class PlayerSetup : NetworkBehaviour {
 
@@ -23,10 +24,32 @@ public class PlayerSetup : NetworkBehaviour {
             GameManager.AddPlayer(gameObject, PlayerNumber, PlayerColor, PlayerName, LocalId);
         OnMyColor(PlayerColor);
     }
+    void OnJoinedRoom()
+    {
+        Debug.Log("Connected to Room");
 
+        // Spawn player
+        //GameObject player = PhotonNetwork.Instantiate(playerPrefab.name, new Vector3(0.5f, 5.0f, 0.5f), Quaternion.identity, 0);
+        var camera = GameObject.FindWithTag("MainCamera");
+        if (camera != null)
+        {
+            Debug.Log("Setting follow target.");
+            var followScript = camera.GetComponent<Camera2DFollow>();
+            if (followScript != null) {
+                followScript.target = transform;
+                Debug.Log("Follow target: " + transform.position.x + "," + transform.position.y);
+            }
+
+        }
+    }
     public override void OnStartLocalPlayer() {
         base.OnStartLocalPlayer();
         CmdColorChanged(PlayerColor);
+
+        OnJoinedRoom();
+
+        //if (GetComponent<NetworkView>().isMine)
+        //    Camera.main.GetComponent<Camera2DFollow>().target = transform;
     }
 
     void OnMyColor(Color newColor) {
