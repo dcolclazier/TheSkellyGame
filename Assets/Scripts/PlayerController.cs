3379f314b4be51094850cdfde666ebf0ad06260f
JR <jaredmreuther@gmail.com>
2016-11-14 23:01:42 -0700

    ++Full Animation + Mobility

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    private
+        Animator anim;
+        Vector3 trans;
+        Rigidbody2D rb;
+    
+    public float Speed = 10f;
+    public float Jump = 1500f;
+        
+	void Start ()
+    {
+        anim = this.GetComponent<Animator>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	void Update ()
+    {
+        if (anim.GetBool("Death") == false)
+        {
+ 
+           anim.SetFloat("Speed", Input.GetAxisRaw("Horizontal"));
+           var x = Input.GetAxisRaw("Horizontal") * Speed * Time.deltaTime;
+           transform.Translate(x, 0, 0);
+        }
+        if(Input.GetKeyDown(KeyCode.E))
+        { 
+            anim.SetBool("Death", true);
+            StartCoroutine(revive(5));
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {                      
+            print("Jump");
+            rb.AddForce(Vector2.up * Jump);
+        }
+    }
+    
+        IEnumerator revive(int time)
+        { 
+            yield return new WaitForSeconds(time);
+           anim.SetBool("Death", false);
+        }
+
+}
