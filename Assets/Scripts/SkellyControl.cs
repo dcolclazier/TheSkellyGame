7b875bd87ea1c7e5366ccf1ac6e27c818f068e38
RaeaEss <raea.ess01@gmail.com>
2016-11-15 16:47:25 -0700

    GameLevelAdd initial port

---
+++ Assets/Scripts/SkellyControl.cs
@@ -0,0 +1,25 @@
+﻿/*using UnityEngine;
+using System.Collections;
+
+public class SkellyControl : MonoBehaviour {
+
+	private Rigidbody2D ridgidBody;
+	public float jumpForce = 6f;
+
+	void Awake(){
+		ridgidBody = GetComponent<Rigidbody2D> ();
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("space")) {
+			Jump ();
+	
+		}
+	}
+	void Jump(){
+		rigidbody.AddForce (Vector2.up * jumpForce, ForceMode2D.Impulse);
+	}
+}
+*/
