50aa8d417bbc7303a9150bb0f41f1d773850a0bb
David Colclazier <david@jdc.tech>
2016-11-21 21:42:06 -0700

    Added swinging trap prefab under Assets\Prefabs\Traps

--- Assets/Swinging_Trap.cs
+++ Assets/Swinging_Trap.cs
@@ -1,18 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class HoleTrap : MonoBehaviour {
+public class Swinging_Trap : MonoBehaviour {
 
-	// Use this for initialization
     public Transform _respawnPoint;
-    private const float RespawnTime = 5;
+    private const float RespawnTime = 3;
+
 
     void OnTriggerEnter2D(Collider2D c) {
-
         var player = GameManager.Instance.GetPlayer(c.gameObject);
         player.Kill(RespawnTime);
         player.SpawnPoint = _respawnPoint;
-
     }
-    
+	
 }
